797a607efef1f3683ed35ccf67dd6c503a7defac
Julian <julox8062@gmail.com>
2026-03-30 19:34:39 +0200

    iniliaze homepage form

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,11 +1,44 @@
 import React from 'react';
-
-const AddTaskPage = () => {
+import styles from "./Home.module.css"
+export default function AddTask({ title, date, time, priority
+})
+{
     return (
        <div className="page-containter">
-             Add Task
+          
+         <div className={styles.contactformcard}>
+          <h2>Add Task</h2>
+
+          <form className={styles.contactform}>
+            <div className={styles.formgroup}>
+              <label>Name</label>
+              <input type="text" className={styles.input} />
+            </div>
+           
+            <div className={styles.formgroup}>
+              <label>Date</label>
+              <input type="date" className={styles.input}/>
+            </div>
+
+             <div className={styles.formgroup}>
+              <label>Time</label>
+              <input type="time" className={styles.input}/>
+            </div>
+
+            <div className={styles.formgroup}>
+              <label>Priority</label>
+             <select>
+              <option >Low</option>
+              <option>Medium</option>
+              <option>High</option>
+             </select>
+            </div>
+
+            <button type="submit" className={styles.btnsendMessage}>
+            Add Task
+            </button>
+          </form>
+        </div>
         </div>
     );
 }
-
-export default AddTaskPage;
