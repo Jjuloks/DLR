bb4f398e0ca532d5e1ba85ecf60216126c8a2049
Julian <julox8062@gmail.com>
2026-03-30 19:49:47 +0200

    simple layout for initial form in addTask

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -4,30 +4,32 @@ export default function AddTask({ title, date, time, priority
 })
 {
     return (
-       <div className="page-containter">
+       <div className={styles.pagecontainer}>
           
          <div className={styles.contactformcard}>
           <h2>Add Task</h2>
-
+<p className={styles.subtitle}>Plan your next step and stay organized</p>
           <form className={styles.contactform}>
             <div className={styles.formgroup}>
               <label>Name</label>
               <input type="text" className={styles.input} />
             </div>
            
-            <div className={styles.formgroup}>
-              <label>Date</label>
-              <input type="date" className={styles.input}/>
-            </div>
+        <div className={styles.row}>
+  <div className={styles.formgroup}>
+    <label>Date</label>
+    <input type="date" className={styles.input}/>
+  </div>
 
-             <div className={styles.formgroup}>
-              <label>Time</label>
-              <input type="time" className={styles.input}/>
-            </div>
+  <div className={styles.formgroup}>
+    <label>Time</label>
+    <input type="time" className={styles.input}/>
+  </div>
+</div>
 
             <div className={styles.formgroup}>
               <label>Priority</label>
-             <select>
+             <select className={styles.input}>
               <option >Low</option>
               <option>Medium</option>
               <option>High</option>
