63b26de087a3bc3b520506f0c58791201fcbfa00
Julian <julox8062@gmail.com>
2026-04-14 22:48:14 +0200

    fixed calendar functionality

--- frontend/src/pages/Calendar/Calendar.jsx
+++ frontend/src/pages/Calendar/Calendar.jsx
@@ -1,10 +1,13 @@
 import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
+import { useState } from 'react'
 
 const localizer = momentLocalizer(moment)
 
 export default function MyCalendar() {
+     const [date, setDate] = useState(new Date())
+    const [view, setView] = useState('month')
   const events = [
     {
       title: 'My Task',
@@ -20,6 +23,10 @@ export default function MyCalendar() {
         events={events}
         startAccessor="start"
         endAccessor="end"
+        date={date}
+        view={view}
+        onNavigate={(newDate) => setDate(newDate)}
+        onView={(newView) => setView(newView)}
       />
     </div>
   )
